01286df23fe6cc3b2247ca244035620fcf7207b6
Jaasiel <69393570+JaasielAntunes@users.noreply.github.com>
2023-05-17 11:31:27 -0300

    Adicionando imagens e criando o efeito blur na tela da aplicação

--- App.tsx
+++ App.tsx
@@ -1,13 +1,34 @@
+/* eslint-disable prettier/prettier */
+import { ImageBackground } from 'react-native'
 import { StatusBar } from 'expo-status-bar'
-import { Text, View } from 'react-native'
+
+import {
+  useFonts,
+  Roboto_400Regular,
+  Roboto_700Bold,
+} from '@expo-google-fonts/roboto'
+
+import { BaiJamjuree_700Bold } from '@expo-google-fonts/bai-jamjuree'
+import blurBg from './src/assets/bg-blur.png'
 
 export default function App() {
+  const [hasLoadedFonts] = useFonts({
+    Roboto_400Regular,
+    Roboto_700Bold,
+    BaiJamjuree_700Bold,
+  })
+
+  if (!hasLoadedFonts) {
+    return null
+  }
+
   return (
-    <View className="flex-1 items-center justify-center bg-gray-900">
-      <Text className="text-4xl font-semibold text-zinc-50">
-        Pronto para codar!!
-      </Text>
-      <StatusBar style="light" />
-    </View>
+    <ImageBackground
+      source={blurBg}
+      className="relative flex-1 items-center bg-gray-900 px-8 py-10"
+      imageStyle={{ position: 'absolute', left: '-100%' }}
+    >
+      <StatusBar style="light" translucent />
+    </ImageBackground>
   )
 }
