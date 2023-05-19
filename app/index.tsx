ddce51d812ea0629f1db817f61d6e703b2798bac
Jaasiel <69393570+JaasielAntunes@users.noreply.github.com>
2023-05-19 11:35:23 -0300

    Configurando rota inicial da aplicação

--- app/index.tsx
+++ app/index.tsx
@@ -1,43 +1,24 @@
 /* eslint-disable react-hooks/exhaustive-deps */
 /* eslint-disable react/jsx-no-undef */
 /* eslint-disable prettier/prettier */
-import { StatusBar } from 'expo-status-bar'
-import { ImageBackground, Text, TouchableOpacity, View } from 'react-native'
-import * as SecureStore from 'expo-secure-store'
-
-import {
-  useFonts,
-  Roboto_400Regular,
-  Roboto_700Bold,
-} from '@expo-google-fonts/roboto'
-
-import { BaiJamjuree_700Bold } from '@expo-google-fonts/bai-jamjuree'
-
-import blurBg from '../src/assets/bg-blur.png'
-import Stripes from '../src/assets/stripes.svg'
-import NLWLogo from '../src/assets/nlw-spacetime-logo.svg'
-import { styled } from 'nativewind'
-import { makeRedirectUri, useAuthRequest } from 'expo-auth-session'
 import { useEffect } from 'react'
 import { useRouter } from 'expo-router'
-import { api } from '../src/lib/api'
+import { Text, TouchableOpacity, View } from 'react-native'
+import { makeRedirectUri, useAuthRequest } from 'expo-auth-session'
+import * as SecureStore from 'expo-secure-store'
 
-const StyledStripes = styled(Stripes)
+import NLWLogo from '../src/assets/nlw-spacetime-logo.svg'
+import { api } from '../src/lib/api'
 
 const discovery = {
   authorizationEndpoint: 'https://github.com/login/oauth/authorize',
   tokenEndpoint: 'https://github.com/login/oauth/access_token',
-  revocationEndpoint: 'https://github.com/settings/connections/applications/<2277fdb029b08458b323>',
-};
+  revocationEndpoint:
+    'https://github.com/settings/connections/applications/2277fdb029b08458b323',
+}
 
 export default function App() {
   const router = useRouter()
-
-  const [hasLoadedFonts] = useFonts({
-    Roboto_400Regular,
-    Roboto_700Bold,
-    BaiJamjuree_700Bold,
-  })
 
   const [, response, signInWithGithub] = useAuthRequest(
     {
@@ -48,7 +29,7 @@ export default function App() {
       }),
     },
     discovery,
-  );
+  )
 
   async function handleGithubOAuthCode(code: string) {
     const response = await api.post('/register', {
@@ -63,34 +44,22 @@ export default function App() {
   }
 
   useEffect(() => {
-    // executar para saber qual endereço IP executar a aplicação em caso de não funcionar
-    // console.log(makeRedirectUri({
-    //   scheme: 'nlwspacetime',
-    // }));
-
-    // console.log(response)
+    // console.log(
+    //   'response',
+    //   makeRedirectUri({
+    //     scheme: 'nlwspacetime',
+    //   }),
+    // )
 
     if (response?.type === 'success') {
-      const { code } = response.params;
+      const { code } = response.params
 
       handleGithubOAuthCode(code)
-
     }
-  }, [response]);
-
-
-  if (!hasLoadedFonts) {
-    return null
-  }
+  }, [response])
 
   return (
-    <ImageBackground
-      source={blurBg}
-      className="relative flex-1 items-center bg-gray-900 px-8 py-10"
-      imageStyle={{ position: 'absolute', left: '-100%' }}
-    >
-      <StyledStripes className="absolute left-2" />
-
+    <View className="flex-1 items-center px-8 py-10">
       <View className="flex-1 items-center justify-center gap-6">
         <NLWLogo />
 
@@ -116,10 +85,8 @@ export default function App() {
       </View>
 
       <Text className="text-center font-body text-sm leading-relaxed text-gray-200">
-        Feito com 💜 by Jaasiel Antunes no NLW da Rocketseat 🚀
+      🚀 Feito com 💜 by Jaasiel Antunes no NLW da Rocketseat 🚀
       </Text>
-
-      <StatusBar style="light" translucent />
-    </ImageBackground>
+    </View>
   )
 }
